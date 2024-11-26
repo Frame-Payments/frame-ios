51805e2aeb033b28d455f669bf5987be90afe80c
Eric Townsend <TOWNSENDEB@YAHOO.COM>
2024-11-26 10:51:24 -0600

    Removed unused code

--- Frame-iOS/Sources/Frame-iOS/Networking/PaymentMethods/PaymentMethodResponses.swift
+++ Frame-iOS/Sources/Frame-iOS/Networking/PaymentMethods/PaymentMethodResponses.swift
@@ -5,9 +5,6 @@
 //  Created by Frame Payments on 9/26/24.
 //
 
-import Foundation
-import EvervaultEnclaves
-
 class PaymentMethodResponses {
     struct ListPaymentMethodsResponse: Decodable {
         let meta: FrameMetadata?
