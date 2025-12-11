ffc48f1c1528d43971a2200e7b84b46602ea91ac
Eric Townsend <eric@framepayments.com>
2025-12-11 14:30:37 -0800

    Update OnboardingContainerView.swift

--- Sources/Frame-Onboarding/Views/OnboardingContainerView.swift
+++ Sources/Frame-Onboarding/Views/OnboardingContainerView.swift
@@ -6,50 +6,69 @@
 //
 
 import SwiftUI
+import Frame_iOS
 
-enum OnboardingFlow: Int, CaseIterable {
+enum OnboardingFlow: Int, CaseIterable, Identifiable {
+    var id: String {
+        return "\(self.rawValue)"
+    }
+
     case selectPaymentMethod = 1
     case confirmPaymentMethod = 2
     case userVerification = 3
+    case onboardingComplete = 4
 }
 
 struct SwiftUIView: View {
-    @State private var onboardingFlow: OnboardingFlow = .selectPaymentMethod
+    @State private var currentStep: OnboardingFlow = .selectPaymentMethod
+    @State private var onboardingFlow: [OnboardingFlow] = [.selectPaymentMethod, .confirmPaymentMethod, .userVerification, .onboardingComplete]
+    @State private var progressiveSteps: [OnboardingFlow] = [.selectPaymentMethod]
     
     let customerId: String
+    
+    init(customerId: String, customOnboardingFlow: [OnboardingFlow]? = nil) {
+        self.customerId = customerId
+        
+        if let customOnboardingFlow, let first = customOnboardingFlow.first {
+            self.onboardingFlow = customOnboardingFlow
+            self.progressiveSteps = [first]
+        }
+    }
     
     var body: some View {
         VStack {
             containerHeader
-                .padding(.bottom)
-            
-            switch onboardingFlow {
+            switch currentStep {
             case .selectPaymentMethod:
                 SelectPaymentMethodView(customerId: customerId)
             case .confirmPaymentMethod:
                 SecurePMVerificationView()
             case .userVerification:
                 UserIdentificationView()
+            case .onboardingComplete:
+                UserIdentificationView()
             }
-            
             Spacer()
-        }.ignoresSafeArea()
+        }
+        .ignoresSafeArea()
     }
     
     var containerHeader: some View {
-        VStack {
-            Rectangle().fill(Color.init(hex: "#325054"))
-                .overlay {
-                    VStack {
-                        Spacer()
-                        Text("Step \(onboardingFlow.rawValue) of \(OnboardingFlow.allCases.count)")
-                            .bold()
-                            .foregroundColor(.white)
-                            .padding()
+        Rectangle()
+            .fill(Color(hex: "#FCFBF8"))
+            .overlay {
+                VStack {
+                    Spacer()
+                    HStack {
+                        ForEach(onboardingFlow) { step in
+                            Image(progressiveSteps.contains(step) ? "filled-onboarding-indicator" : "empty-onboarding-indicator", bundle: FrameResources.module)
+                        }
                     }
+                    .padding(.bottom, 15.0)
+                    Divider()
                 }
-        }
-        .frame(height: 100.0)
+            }
+            .frame(height: 100.0)
     }
 }
 
