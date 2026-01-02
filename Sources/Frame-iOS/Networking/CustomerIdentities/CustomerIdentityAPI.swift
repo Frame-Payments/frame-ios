0e69cbee671a0ba171e64c5091a52f2b010b24aa
Eric Townsend <eric@framepayments.com>
2026-01-02 11:34:18 -0800

    Added new Identity Verification endpoints

--- Sources/Frame-iOS/Networking/CustomerIdentities/CustomerIdentityAPI.swift
+++ Sources/Frame-iOS/Networking/CustomerIdentities/CustomerIdentityAPI.swift
@@ -11,11 +11,17 @@ import Foundation
 protocol CustomerIdentityProtocol {
     //async/await
     static func createCustomerIdentity(request: CustomerIdentityRequest.CreateCustomerIdentityRequest) async throws -> (FrameObjects.CustomerIdentity?, NetworkingError?)
+    static func createCustomerIdentityWith(customerId: String) async throws -> (FrameObjects.CustomerIdentity?, NetworkingError?)
     static func getCustomerIdentityWith(customerIdentityId: String) async throws -> (FrameObjects.CustomerIdentity?, NetworkingError?)
+    static func submitForVerification(customerIdentityId: String) async throws -> (FrameObjects.CustomerIdentity?, NetworkingError?)
+    static func uploadIdentityDocuments(customerIdentityId: String) async throws -> (FrameObjects.CustomerIdentity?, NetworkingError?)
     
     // completionHandlers
     static func createCustomerIdentity(request: CustomerIdentityRequest.CreateCustomerIdentityRequest, completionHandler: @escaping @Sendable (FrameObjects.CustomerIdentity?, NetworkingError?) -> Void)
+    static func createCustomerIdentityWith(customerId: String, completionHandler: @escaping @Sendable (FrameObjects.CustomerIdentity?, NetworkingError?) -> Void)
     static func getCustomerIdentityWith(customerIdentityId: String, completionHandler: @escaping @Sendable (FrameObjects.CustomerIdentity?, NetworkingError?) -> Void)
+    static func submitForVerification(customerIdentityId: String, completionHandler: @escaping @Sendable (FrameObjects.CustomerIdentity?, NetworkingError?) -> Void)
+    static func uploadIdentityDocuments(customerIdentityId: String, completionHandler: @escaping @Sendable (FrameObjects.CustomerIdentity?, NetworkingError?) -> Void)
 }
 
 // Customer Identity API
@@ -34,9 +40,45 @@ public class CustomerIdentityAPI: CustomerIdentityProtocol, @unchecked Sendable 
         }
     }
     
+    public static func createCustomerIdentityWith(customerId: String) async throws -> (FrameObjects.CustomerIdentity?, NetworkingError?) {
+       guard !customerIdentityId.isEmpty else { return (nil, nil) }
+        let endpoint = CustomerIdentityEndpoints.createCustomerIdenityWith(customerId: customerId)
+        
+        let (data, error) = try await FrameNetworking.shared.performDataTask(endpoint: endpoint)
+        if let data, let decodedResponse = try? FrameNetworking.shared.jsonDecoder.decode(FrameObjects.CustomerIdentity.self, from: data) {
+            return (decodedResponse, error)
+        } else {
+            return (nil, error)
+        }
+    }
+    
     public static func getCustomerIdentityWith(customerIdentityId: String) async throws -> (FrameObjects.CustomerIdentity?, NetworkingError?) {
        guard !customerIdentityId.isEmpty else { return (nil, nil) }
         let endpoint = CustomerIdentityEndpoints.getCustomerIdentityWith(customerIdentityId: customerIdentityId)
+        
+        let (data, error) = try await FrameNetworking.shared.performDataTask(endpoint: endpoint)
+        if let data, let decodedResponse = try? FrameNetworking.shared.jsonDecoder.decode(FrameObjects.CustomerIdentity.self, from: data) {
+            return (decodedResponse, error)
+        } else {
+            return (nil, error)
+        }
+    }
+    
+    public static func submitForVerification(customerIdentityId: String) async throws -> (FrameObjects.CustomerIdentity?, NetworkingError?) {
+       guard !customerIdentityId.isEmpty else { return (nil, nil) }
+        let endpoint = CustomerIdentityEndpoints.submitForVerification(customerIdentityId: customerIdentityId)
+        
+        let (data, error) = try await FrameNetworking.shared.performDataTask(endpoint: endpoint)
+        if let data, let decodedResponse = try? FrameNetworking.shared.jsonDecoder.decode(FrameObjects.CustomerIdentity.self, from: data) {
+            return (decodedResponse, error)
+        } else {
+            return (nil, error)
+        }
+    }
+    
+    public static func uploadIdentityDocuments(customerIdentityId: String) async throws -> (FrameObjects.CustomerIdentity?, NetworkingError?) {
+       guard !customerIdentityId.isEmpty else { return (nil, nil) }
+        let endpoint = CustomerIdentityEndpoints.uploadIdentityDocuments(customerIdentityId: customerIdentityId)
         
         let (data, error) = try await FrameNetworking.shared.performDataTask(endpoint: endpoint)
         if let data, let decodedResponse = try? FrameNetworking.shared.jsonDecoder.decode(FrameObjects.CustomerIdentity.self, from: data) {
@@ -60,9 +102,48 @@ public class CustomerIdentityAPI: CustomerIdentityProtocol, @unchecked Sendable 
         }
     }
     
+    public static func createCustomerIdentityWith(customerId: String, completionHandler: @escaping @Sendable (FrameObjects.CustomerIdentity?, NetworkingError?) -> Void) {
+        guard !cus.isEmpty else { return completionHandler(nil, nil) }
+        let endpoint = CustomerIdentityEndpoints.createCustomerIdenityWith(customerId: customerId)
+        
+        FrameNetworking.shared.performDataTask(endpoint: endpoint) { data, response, error in
+            if let data, let decodedResponse = try? FrameNetworking.shared.jsonDecoder.decode(FrameObjects.CustomerIdentity.self, from: data) {
+                completionHandler(decodedResponse, error)
+            } else {
+                completionHandler(nil, error)
+            }
+        }
+    }
+    
     public static func getCustomerIdentityWith(customerIdentityId: String, completionHandler: @escaping @Sendable (FrameObjects.CustomerIdentity?, NetworkingError?) -> Void) {
         guard !customerIdentityId.isEmpty else { return completionHandler(nil, nil) }
         let endpoint = CustomerIdentityEndpoints.getCustomerIdentityWith(customerIdentityId: customerIdentityId)
+        
+        FrameNetworking.shared.performDataTask(endpoint: endpoint) { data, response, error in
+            if let data, let decodedResponse = try? FrameNetworking.shared.jsonDecoder.decode(FrameObjects.CustomerIdentity.self, from: data) {
+                completionHandler(decodedResponse, error)
+            } else {
+                completionHandler(nil, error)
+            }
+        }
+    }
+    
+    public static func submitForVerification(customerIdentityId: String, completionHandler: @escaping @Sendable (FrameObjects.CustomerIdentity?, NetworkingError?) -> Void) {
+        guard !customerIdentityId.isEmpty else { return completionHandler(nil, nil) }
+        let endpoint = CustomerIdentityEndpoints.submitForVerification(customerIdentityId: customerIdentityId)
+        
+        FrameNetworking.shared.performDataTask(endpoint: endpoint) { data, response, error in
+            if let data, let decodedResponse = try? FrameNetworking.shared.jsonDecoder.decode(FrameObjects.CustomerIdentity.self, from: data) {
+                completionHandler(decodedResponse, error)
+            } else {
+                completionHandler(nil, error)
+            }
+        }
+    }
+    
+    public static func uploadIdentityDocuments(customerIdentityId: String, completionHandler: @escaping @Sendable (FrameObjects.CustomerIdentity?, NetworkingError?) -> Void) {
+        guard !customerIdentityId.isEmpty else { return completionHandler(nil, nil) }
+        let endpoint = CustomerIdentityEndpoints.uploadIdentityDocuments(customerIdentityId: customerIdentityId)
         
         FrameNetworking.shared.performDataTask(endpoint: endpoint) { data, response, error in
             if let data, let decodedResponse = try? FrameNetworking.shared.jsonDecoder.decode(FrameObjects.CustomerIdentity.self, from: data) {
